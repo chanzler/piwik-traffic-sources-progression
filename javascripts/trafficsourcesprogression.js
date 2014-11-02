ba1bfdf88bb7b755a074f3aff5694e48ccdd872d
Frank Kanzler <frank@intersolve.de>
2014-11-02 20:27:34 +0100

    s

--- javascripts/trafficsourcesprogression.js
+++ javascripts/trafficsourcesprogression.js
@@ -58,7 +58,7 @@ $(function() {
 			});
 	        
 	        ajaxRequest.send(true);
-			setTimeout(update(updateInterval), updateInterval);
+			setTimeout(function() { update(updateInterval); }, updateInterval);
 		}
 
 	});
