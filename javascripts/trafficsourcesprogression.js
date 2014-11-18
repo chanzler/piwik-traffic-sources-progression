e0ea62a35a53c44b5b1d0443f101ee44e2d5a2d3
Frank Kanzler <frank@intersolve.de>
2014-11-18 20:53:56 +0100

    s

--- javascripts/trafficsourcesprogression.js
+++ javascripts/trafficsourcesprogression.js
@@ -80,7 +80,7 @@ $(function() {
 				y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
 			}
 
-			legends.eq(i).text(series.label.replace(/=.*/, "= " + y));
+			legends.eq(i).text(series.label.replace(/=.*/, "= " + y.toFixed(0)));
 		}
 	}
 
